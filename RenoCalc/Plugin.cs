534e10a08d929ecd7e3f5ebbd1ca39ebe014b5bd
theAprel <theAprel@example.fake>
2015-12-08 01:03:41 -0500

    Initial commit

---
+++ RenoCalc/Plugin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Controls;
+
+using Hearthstone_Deck_Tracker.Plugins;
+
+namespace RenoCalc
+{
+    public class Plugin : IPlugin
+    {
+		public string Author
+		{
+			get { return "Aprel"; }
+		}
+
+		public string ButtonText
+		{
+			get { return "RenoCalc"; }
+		}
+
+		public string Description
+		{
+			get { return "Displays a simple window UI with probabilites associated with Reno Jackson's battlecry activation."; }
+		}
+
+		public MenuItem MenuItem
+		{
+			get { return null; }
+		}
+
+		public string Name
+		{
+			get { return "RenoCalc"; }
+		}
+
+		public void OnButtonPress()
+		{
+		}
+
+		public void OnLoad()
+		{
+            Calculator.Load();
+		}
+
+		public void OnUnload()
+		{
+		}
+
+		public void OnUpdate()
+		{
+		}
+
+		public Version Version
+		{
+			get { return new Version(0, 0, 1); }
+		}
+    }
+}
---
+++ RenoCalc/RenoCalc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenoCalc
+{
+    public class Calculator
+    {
+
+        public static void Load()
+        {
+            // Do something
+        }
+    }
+}
